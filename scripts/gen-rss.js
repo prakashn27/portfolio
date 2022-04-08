4604bdc2dcf904eaf29e8abf4f283a3e782d617f
Prakash Natarajan <nprakashece@gmail.com>
2022-04-08 18:37:18 -0400

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Prakash Natarajan',
+    site_url: 'https://prakashn.com',
+    feed_url: 'https://prakashn.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
